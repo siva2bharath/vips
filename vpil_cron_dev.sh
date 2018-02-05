1158d93e34deb9bde1ea5e91035471c13d9d03a3
siva2bharath <33661434+siva2bharath@users.noreply.github.com>
2018-02-05 10:53:51 -0500

    Add files via upload

---
+++ vpil_cron_dev.sh
@@ -0,0 +1,10 @@
+byWho="CronJob"
+CID=`date '+%m%d%y%H%M'`
+for partner in $(curl "https://venuerpil-dev.ccp.xcal.tv:10652/venuerpil/partners?source=hms&requestedBy=$byWho&cid=$CID" |awk 'NR > 1 {print $1}' RS='[' FS=']' |sed "s/,/ /g" | tr -d '"')
+do
+	echo "Working on partner id : $partner"
+	CID=`date '+%m%d%y%H%M'`
+
+	curl  -vv -X PUT  "https://venuerpil-dev.ccp.xcal.tv:10652/venuerpil/partner/devices/xapistatus?partnerCode=$partner&requestedBy=$byWho&cid=$CID"
+done
+
